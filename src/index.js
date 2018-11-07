52dfb7129c43a9778cc14b08d1484b6d3ff94f11
Nikolay Gromov <nicothin@gmail.com>
2018-11-07 17:09:54 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import style from "./scss/style.scss";
+
+
+
+console.log(`I've been required by Webpack`);
+
+let dd = 'eeeeee';
+console.log(`eeeeeeeeeeeee ${dd}`);
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+
+  output: {
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            // options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g)/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              name: "./img/[name].[ext]",
+              limit: 10000
+            }
+          },
+          {
+            loader: "img-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebPackPlugin({
+      template: "src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",  // [hash].
+      chunkFilename: "[id].css"
+    })
+  ]
+
+};
